317d0c6e389fa74932f24d920a9fd8a09a0e80b2
Blevenson <blevenson68@gmail.com>
2016-07-08 12:53:34 -0700

    Adds RobotProvider and sets correct hardware adapter

--- edu/wpi/first/wpilibj/RobotBase.java
+++ edu/wpi/first/wpilibj/RobotBase.java
@@ -7,6 +7,8 @@
 
 package edu.wpi.first.wpilibj;
 
+import interfaces.RobotProvider;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -36,225 +38,237 @@ import edu.wpi.first.wpilibj.Utility;
  * Autonomous period.
  */
 public abstract class RobotBase {
-  /**
-   * The VxWorks priority that robot code should work at (so Java code should
-   * run at)
-   */
-  public static final int ROBOT_TASK_PRIORITY = 101;
+	/**
+	 * The VxWorks priority that robot code should work at (so Java code should
+	 * run at)
+	 */
+	public static final int ROBOT_TASK_PRIORITY = 101;
 
-  protected final DriverStation m_ds;
+	protected final DriverStation m_ds;
 
-  /**
-   * Constructor for a generic robot program. User code should be placed in the
-   * constructor that runs before the Autonomous or Operator Control period
-   * starts. The constructor will run to completion before Autonomous is
-   * entered.
-   *
-   * This must be used to ensure that the communications code starts. In the
-   * future it would be nice to put this code into it's own task that loads on
-   * boot so ensure that it runs.
-   */
-  protected RobotBase() {
-    // TODO: StartCAPI();
-    // TODO: See if the next line is necessary
-    // Resource.RestartProgram();
+	/**
+	 * Constructor for a generic robot program. User code should be placed in
+	 * the constructor that runs before the Autonomous or Operator Control
+	 * period starts. The constructor will run to completion before Autonomous
+	 * is entered.
+	 *
+	 * This must be used to ensure that the communications code starts. In the
+	 * future it would be nice to put this code into it's own task that loads on
+	 * boot so ensure that it runs.
+	 */
+	protected RobotBase() {
+		// TODO: StartCAPI();
+		// TODO: See if the next line is necessary
+		// Resource.RestartProgram();
 
-    NetworkTable.setNetworkIdentity("Robot");
-    NetworkTable.setPersistentFilename("/home/lvuser/networktables.ini");
-    NetworkTable.setServerMode();// must be before b
-    m_ds = DriverStation.getInstance();
-    NetworkTable.getTable(""); // forces network tables to initialize
-    NetworkTable.getTable("LiveWindow").getSubTable("~STATUS~").putBoolean("LW Enabled", false);
-  }
+		NetworkTable.setNetworkIdentity("Robot");
+		NetworkTable.setPersistentFilename("/home/lvuser/networktables.ini");
+		NetworkTable.setServerMode();// must be before b
+		m_ds = DriverStation.getInstance();
+		NetworkTable.getTable(""); // forces network tables to initialize
+		NetworkTable.getTable("LiveWindow").getSubTable("~STATUS~")
+				.putBoolean("LW Enabled", false);
+	}
 
-  /**
-   * Free the resources for a RobotBase class.
-   */
-  public void free() {}
+	/**
+	 * Free the resources for a RobotBase class.
+	 */
+	public void free() {
+	}
 
-  /**
-   * @return If the robot is running in simulation.
-   */
-  public static boolean isSimulation() {
-    return false;
-  }
+	/**
+	 * @return If the robot is running in simulation.
+	 */
+	public static boolean isSimulation() {
+		return false;
+	}
 
-  /**
-   * @return If the robot is running in the real world.
-   */
-  public static boolean isReal() {
-    return true;
-  }
+	/**
+	 * @return If the robot is running in the real world.
+	 */
+	public static boolean isReal() {
+		return true;
+	}
 
-  /**
-   * Determine if the Robot is currently disabled.
-   *$
-   * @return True if the Robot is currently disabled by the field controls.
-   */
-  public boolean isDisabled() {
-    return m_ds.isDisabled();
-  }
+	/**
+	 * Determine if the Robot is currently disabled. $
+	 * 
+	 * @return True if the Robot is currently disabled by the field controls.
+	 */
+	public boolean isDisabled() {
+		return m_ds.isDisabled();
+	}
 
-  /**
-   * Determine if the Robot is currently enabled.
-   *$
-   * @return True if the Robot is currently enabled by the field controls.
-   */
-  public boolean isEnabled() {
-    return m_ds.isEnabled();
-  }
+	/**
+	 * Determine if the Robot is currently enabled. $
+	 * 
+	 * @return True if the Robot is currently enabled by the field controls.
+	 */
+	public boolean isEnabled() {
+		return m_ds.isEnabled();
+	}
 
-  /**
-   * Determine if the robot is currently in Autonomous mode.
-   *$
-   * @return True if the robot is currently operating Autonomously as determined
-   *         by the field controls.
-   */
-  public boolean isAutonomous() {
-    return m_ds.isAutonomous();
-  }
+	/**
+	 * Determine if the robot is currently in Autonomous mode. $
+	 * 
+	 * @return True if the robot is currently operating Autonomously as
+	 *         determined by the field controls.
+	 */
+	public boolean isAutonomous() {
+		return m_ds.isAutonomous();
+	}
 
-  /**
-   * Determine if the robot is currently in Test mode
-   *$
-   * @return True if the robot is currently operating in Test mode as determined
-   *         by the driver station.
-   */
-  public boolean isTest() {
-    return m_ds.isTest();
-  }
+	/**
+	 * Determine if the robot is currently in Test mode $
+	 * 
+	 * @return True if the robot is currently operating in Test mode as
+	 *         determined by the driver station.
+	 */
+	public boolean isTest() {
+		return m_ds.isTest();
+	}
 
-  /**
-   * Determine if the robot is currently in Operator Control mode.
-   *$
-   * @return True if the robot is currently operating in Tele-Op mode as
-   *         determined by the field controls.
-   */
-  public boolean isOperatorControl() {
-    return m_ds.isOperatorControl();
-  }
+	/**
+	 * Determine if the robot is currently in Operator Control mode. $
+	 * 
+	 * @return True if the robot is currently operating in Tele-Op mode as
+	 *         determined by the field controls.
+	 */
+	public boolean isOperatorControl() {
+		return m_ds.isOperatorControl();
+	}
 
-  /**
-   * Indicates if new data is available from the driver station.
-   *$
-   * @return Has new data arrived over the network since the last time this
-   *         function was called?
-   */
-  public boolean isNewDataAvailable() {
-    return m_ds.isNewControlData();
-  }
+	/**
+	 * Indicates if new data is available from the driver station. $
+	 * 
+	 * @return Has new data arrived over the network since the last time this
+	 *         function was called?
+	 */
+	public boolean isNewDataAvailable() {
+		return m_ds.isNewControlData();
+	}
 
-  /**
-   * Provide an alternate "main loop" via startCompetition().
-   */
-  public abstract void startCompetition();
+	/**
+	 * Provide an alternate "main loop" via startCompetition().
+	 */
+	public abstract void startCompetition();
 
-  public static boolean getBooleanProperty(String name, boolean defaultValue) {
-    String propVal = System.getProperty(name);
-    if (propVal == null) {
-      return defaultValue;
-    }
-    if (propVal.equalsIgnoreCase("false")) {
-      return false;
-    } else if (propVal.equalsIgnoreCase("true")) {
-      return true;
-    } else {
-      throw new IllegalStateException(propVal);
-    }
-  }
+	public static boolean getBooleanProperty(String name, boolean defaultValue) {
+		String propVal = System.getProperty(name);
+		if (propVal == null) {
+			return defaultValue;
+		}
+		if (propVal.equalsIgnoreCase("false")) {
+			return false;
+		} else if (propVal.equalsIgnoreCase("true")) {
+			return true;
+		} else {
+			throw new IllegalStateException(propVal);
+		}
+	}
 
-  /**
-   * Common initialization for all robot programs.
-   */
-  public static void initializeHardwareConfiguration() {
-    FRCNetworkCommunicationsLibrary.FRCNetworkCommunicationReserve();
+	/**
+	 * Common initialization for all robot programs.
+	 */
+	public static void initializeHardwareConfiguration() {
+		FRCNetworkCommunicationsLibrary.FRCNetworkCommunicationReserve();
 
-    // Set some implementations so that the static methods work properly
-    Timer.SetImplementation(new HardwareTimer());
-    HLUsageReporting.SetImplementation(new HardwareHLUsageReporting());
-    RobotState.SetImplementation(DriverStation.getInstance());
-  }
+		// Set some implementations so that the static methods work properly
+		Timer.SetImplementation(new HardwareTimer());
+		HLUsageReporting.SetImplementation(new HardwareHLUsageReporting());
+		RobotState.SetImplementation(DriverStation.getInstance());
+	}
 
-  /**
-   * Starting point for the applications.
-   */
-  public static void main(String args[]) {
-    initializeHardwareConfiguration();
+	/**
+	 * Starting point for the applications.
+	 */
+	public static void main(String args[]) {
+		RobotProvider.instance = new WPIRobotProvider();
+		
+		initializeHardwareConfiguration();
 
-    UsageReporting.report(tResourceType.kResourceType_Language, tInstances.kLanguage_Java);
+		UsageReporting.report(tResourceType.kResourceType_Language,
+				tInstances.kLanguage_Java);
 
-    String robotName = "";
-    Enumeration<URL> resources = null;
-    try {
-      resources = RobotBase.class.getClassLoader().getResources("META-INF/MANIFEST.MF");
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    while (resources != null && resources.hasMoreElements()) {
-      try {
-        Manifest manifest = new Manifest(resources.nextElement().openStream());
-        robotName = manifest.getMainAttributes().getValue("Robot-Class");
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-    }
+		String robotName = "";
+		Enumeration<URL> resources = null;
+		try {
+			resources = RobotBase.class.getClassLoader().getResources(
+					"META-INF/MANIFEST.MF");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		while (resources != null && resources.hasMoreElements()) {
+			try {
+				Manifest manifest = new Manifest(resources.nextElement()
+						.openStream());
+				robotName = manifest.getMainAttributes()
+						.getValue("Robot-Class");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 
-    RobotBase robot;
-    try {
-      robot = (RobotBase) Class.forName(robotName).newInstance();
-    } catch (Throwable t) {
-      DriverStation.reportError("ERROR Unhandled exception instantiating robot " + robotName + " "
-          + t.toString() + " at " + Arrays.toString(t.getStackTrace()), false);
-      System.err.println("WARNING: Robots don't quit!");
-      System.err.println("ERROR: Could not instantiate robot " + robotName + "!");
-      System.exit(1);
-      return;
-    }
+		RobotBase robot;
+		try {
+			robot = (RobotBase) Class.forName(robotName).newInstance();
+		} catch (Throwable t) {
+			DriverStation.reportError(
+					"ERROR Unhandled exception instantiating robot "
+							+ robotName + " " + t.toString() + " at "
+							+ Arrays.toString(t.getStackTrace()), false);
+			System.err.println("WARNING: Robots don't quit!");
+			System.err.println("ERROR: Could not instantiate robot "
+					+ robotName + "!");
+			System.exit(1);
+			return;
+		}
 
-    File file = null;
-    FileOutputStream output = null;
-    try {
-      file = new File("/tmp/frc_versions/FRC_Lib_Version.ini");
+		File file = null;
+		FileOutputStream output = null;
+		try {
+			file = new File("/tmp/frc_versions/FRC_Lib_Version.ini");
 
-      if (file.exists())
-        file.delete();
+			if (file.exists())
+				file.delete();
 
-      file.createNewFile();
+			file.createNewFile();
 
-      output = new FileOutputStream(file);
+			output = new FileOutputStream(file);
 
-      output.write("2016 Java Release 4".getBytes());
+			output.write("2016 Java Release 4".getBytes());
 
-    } catch (IOException ex) {
-      ex.printStackTrace();
-    } finally {
-      if (output != null) {
-        try {
-          output.close();
-        } catch (IOException ex) {
-        }
-      }
-    }
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (output != null) {
+				try {
+					output.close();
+				} catch (IOException ex) {
+				}
+			}
+		}
 
-    boolean errorOnExit = false;
-    try {
-      System.out.println("********** Robot program starting **********");
-      robot.startCompetition();
-    } catch (Throwable t) {
-      DriverStation.reportError(
-          "ERROR Unhandled exception: " + t.toString() + " at "
-              + Arrays.toString(t.getStackTrace()), false);
-      errorOnExit = true;
-    } finally {
-      // startCompetition never returns unless exception occurs....
-      System.err.println("WARNING: Robots don't quit!");
-      if (errorOnExit) {
-        System.err
-            .println("---> The startCompetition() method (or methods called by it) should have handled the exception above.");
-      } else {
-        System.err.println("---> Unexpected return from startCompetition() method.");
-      }
-    }
-    System.exit(1);
-  }
+		boolean errorOnExit = false;
+		try {
+			System.out.println("********** Robot program starting **********");
+			robot.startCompetition();
+		} catch (Throwable t) {
+			DriverStation.reportError(
+					"ERROR Unhandled exception: " + t.toString() + " at "
+							+ Arrays.toString(t.getStackTrace()), false);
+			errorOnExit = true;
+		} finally {
+			// startCompetition never returns unless exception occurs....
+			System.err.println("WARNING: Robots don't quit!");
+			if (errorOnExit) {
+				System.err
+						.println("---> The startCompetition() method (or methods called by it) should have handled the exception above.");
+			} else {
+				System.err
+						.println("---> Unexpected return from startCompetition() method.");
+			}
+		}
+		System.exit(1);
+	}
 }
---
+++ edu/wpi/first/wpilibj/WPIRobotProvider.java
@@ -0,0 +1,54 @@
+package edu.wpi.first.wpilibj;
+
+import interfaces.EncoderReader;
+import interfaces.GyroReader;
+import interfaces.RobotProvider;
+import interfaces.SolenoidController;
+import interfaces.SpeedController;
+
+public class WPIRobotProvider extends RobotProvider{
+
+	/**
+	 * Ports
+	 */
+	private final int LEFT_DRIVE = 1,
+					  RIGHT_DRIVE = 0,
+					  DIO_LDriveEncA = 16,	//Enc3
+					  DIO_LDriveEncB = 17,
+					  DIO_RDriveEncA = 14,	//Enc2
+					  DIO_RDriveEncB = 15,
+					  SOL_DRIVE_SHIFT = 0;
+	
+	
+	
+	@Override
+	public SpeedController getLeftDrive() {
+		return new Talon(LEFT_DRIVE);
+	}
+
+	@Override
+	public SpeedController getRightDrive() {
+		return new Talon(RIGHT_DRIVE);
+	}
+
+	@Override
+	public EncoderReader getLeftEncoder() {
+		return new Encoder(DIO_LDriveEncA, DIO_LDriveEncB);
+	}
+
+	@Override
+	public EncoderReader getRightEncoder() {
+		return new Encoder(DIO_RDriveEncA, DIO_RDriveEncB);
+	}
+
+	@Override
+	public SolenoidController getDriveShifter() {
+		return new Solenoid(0, SOL_DRIVE_SHIFT);
+	}
+
+	@Override
+	public GyroReader getGyro() {
+		return new ADXRS450_Gyro(SPI.Port.kOnboardCS0);
+	}
+
+}
