0c28266dbc0b34ce57bf24f5364300ba868aa5c3
blevenson <blevenson68@gmail.com>
2016-11-05 14:58:52 -0700

    Prevents reallocating ports

--- src/edu/wpi/first/wpilibj/WPIRobotProvider.java
+++ src/edu/wpi/first/wpilibj/WPIRobotProvider.java
@@ -14,22 +14,30 @@ public class WPIRobotProvider extends RobotProvider{
 
 	@Override
 	public SpeedController getMotor(int index) {
-		return new Talon(index);
+		if(motors[index] == null)
+			motors[index] = new Talon(index);
+		return motors[index];
 	}
 
 	@Override
 	public EncoderReader getEncoder(int index1, int index2) {
-		return new Encoder(index1, index2);
+		if(encoders[index1] == null)
+			encoders[index1] = new Encoder(index1, index2);
+		return encoders[index1];
 	}
 
 	@Override
 	public SolenoidController getSolenoid(int index) {
-		return new Solenoid(index);
+		if(solenoids[index] == null)
+			solenoids[index] = new Solenoid(index);
+		return solenoids[index];
 	}
 
 	@Override
 	public GyroReader getGyro(int index) {
-		return new ADXRS450_Gyro(SPI.Port.kOnboardCS0);
+		if(gyros[0] == null)
+			gyros[0] = new ADXRS450_Gyro(SPI.Port.kOnboardCS0);
+		return gyros[0];
 	}
 	
 	@Override
@@ -41,7 +49,9 @@ public class WPIRobotProvider extends RobotProvider{
 
 	@Override
 	public JoystickReader getJoystick(int index) {
-		return new Joystick(index);
+		if(joysticks[index] == null)
+			joysticks[index] = new Joystick(index);
+		return joysticks[index];
 	}
 
 }
